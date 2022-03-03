caa00b6f624879d24b93f64aa9776cb534086ace
KangHyeoni <rkdgus0410@naver.com>
2022-03-04 00:04:20 +0900

    Feat: Add ping-pong api

--- SNUDY/SNUDY/settings.py
+++ SNUDY/SNUDY/settings.py
@@ -50,6 +50,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django_filters',
+    'drf_yasg',
+    'rest_framework',
+    'rest_framework_jwt',
+    'rest_framework.authtoken',
+    'ping.apps.PingConfig'
 ]
 
 MIDDLEWARE = [
