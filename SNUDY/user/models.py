1a79a875f0166798a9ff23df050bed057529c0a2
KangHyeoni <rkdgus0410@naver.com>
2022-03-23 18:12:51 +0900

    Feat: User model

---
+++ SNUDY/user/models.py
@@ -0,0 +1,44 @@
+import os
+import hmac
+import time
+import base64
+import hashlib
+import datetime
+import requests
+from random import randint
+
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
+from django.db import models
+from django.utils import timezone
+
+
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, **extra_fields):
+        user = self.model(**extra_fields)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, password, **extra_fields):
+        user = self.model(**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    USERNAME_FIELD = "email"
+
+    name = models.CharField(max_length=32)
+    email = models.EmailField(max_length=128, unique=True)
+    student_id = models.CharField(max_length=10, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    is_admin = models.BooleanField(null=True, default=False)
+
+    objects = CustomUserManager()
