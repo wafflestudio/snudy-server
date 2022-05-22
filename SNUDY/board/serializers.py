627d2c4e7ba7d6243df27b604e0ffcb7ad581d0a
KangHyeoni <rkdgus0410@naver.com>
2022-05-23 03:04:47 +0900

    Feat: Add post get/update api

--- SNUDY/board/serializers.py
+++ SNUDY/board/serializers.py
@@ -21,9 +21,38 @@ class PostCreateSerializer(serializers.Serializer):
         return data
 
     def create(self, validated_data):
-        user = self.context["user"]
-        board_id = self.context["board_id"]
+        user = self.context.get("user")
+        board_id = self.context.get("board_id")
         board = get_object_or_404(Board, pk=board_id)
 
         post = Post.objects.create(writer=user, board=board, **validated_data)
         return post
+
+    def update(self, post, validated_data):
+        post.title = validated_data.get("title", post.title)
+        post.content = validated_data.get("content", post.content)
+        post.is_anonymous = validated_data.get("is_anonymous", post.is_anonymous)
+        post.save()
+        return post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    writer = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = (
+            "id",
+            "title",
+            "content",
+            "writer",
+            "is_anonymous",
+            "created_at",
+            "updated_at",
+        )
+
+    def get_writer(self, post):
+        if post.is_anonymous:
+            return "익명"
+        else:
+            return post.writer.name
--- SNUDY/board/views.py
+++ SNUDY/board/views.py
@@ -4,11 +4,12 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 from board.models import Post
-from board.serializers import PostCreateSerializer
+from board.serializers import PostCreateSerializer, PostSerializer
 
 
 class PostViewSet(viewsets.GenericViewSet):
     permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = PostSerializer
     queryset = Post.objects.all()
 
     def create(self, request, b_pk=None):
@@ -17,8 +18,31 @@ class PostViewSet(viewsets.GenericViewSet):
             data=request.data, context={"user": user, "board_id": b_pk}
         )
         serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(status=status.HTTP_201_CREATED)
+        post = serializer.save()
+        return Response(
+            data=self.get_serializer(post).data, status=status.HTTP_201_CREATED
+        )
+
+    def retrieve(self, request, pk=None, **kwargs):
+        post = get_object_or_404(self.queryset, pk=pk)
+        return Response(
+            data=self.get_serializer(post).data, status=status.HTTP_201_CREATED
+        )
+
+    def update(self, request, b_pk=None, pk=None):
+        user = request.user
+        post = get_object_or_404(self.queryset, pk=pk)
+        if user != post.writer:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        serializer = PostCreateSerializer(
+            post, data=request.data, context={"user": user, "board_id": b_pk}
+        )
+        serializer.is_valid(raise_exception=True)
+        post = serializer.save()
+        return Response(
+            data=self.get_serializer(post).data, status=status.HTTP_201_CREATED
+        )
 
     def destroy(self, request, pk=None, **kwargs):
         user = request.user
